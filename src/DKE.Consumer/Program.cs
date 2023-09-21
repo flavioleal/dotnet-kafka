fc8a44062cc4c86e951f21495b26d83b145271ce
Flavio Leal <flasile@gmail.com>
2023-09-20 22:48:19 -0300

    commit inicial

---
+++ src/DKE.Consumer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "dkexample",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+consumer.Subscribe("topico-teste");
+
+while (true)
+{
+    var result = consumer.Consume();
+    System.Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+}
---
+++ src/DKE.Producer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = $"Mensagem de teste {DateTime.Now.Second}"
+};
+
+var result = await producer.ProduceAsync("topico-teste", message);
+
+System.Console.WriteLine($"{result.Offset}");
